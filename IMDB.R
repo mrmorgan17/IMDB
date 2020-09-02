a2e31d11f58a6642475bc591e11f379b2d81592b
Matthew Morgan <matt4byu@gmail.com>
2020-09-02 16:12:29 -0600

    More changes

--- IMDB.R
+++ IMDB.R
@@ -1,16 +1,5 @@
-library(caret)
-library(dplyr)
+library(tidyverse)
 
 train <- read.csv("IMDBTrain.csv", header = TRUE)
 test <- read.csv("IMDBTest.csv", header = TRUE)
-sample <- read.csv("SampleSubmission.csv", header = TRUE)
 
-train <- train %>% na.omit() %>% select(-movie_imdb_link)
-
-fit <- randomForest::randomForest(train$imdb_score ~., data = train)
-
-submission <- data.frame(Id = sample$Id, Predicted = predict(fit, test))
-
-submission[is.na(submission$Predicted),]$Predicted <- mean(submission[!is.na(submission$Predicted),]$Predicted)
-
-write.csv(submission, "rf.csv", row.names = FALSE)
